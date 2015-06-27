3516294d3aeb3226f36cfb76e250e7c9a1148c1b
Yusuke Nakamura <yusuke1994525@gmail.com>
2015-06-27 23:49:23 +0900

    generate spina
    $ rails g spina:install

---
+++ config/initializers/spina.rb
@@ -0,0 +1,10 @@
+Spina::Engine.configure do
+  config.NEGATIVE_CAPTCHA_SECRET = 'd534b238b1cbea6cc3582cae395c2e8bbc1cd2d8a5f2b97b37db328f1056c315b473cbcec1283f5ccb82fb2ec976dd43c4b77f16412ef88e6ca6713cc356cc3b'
+
+  # If you want to use s3 to store uploads
+  # config.storage = :s3
+  # config.aws_region = "eu-west-1"
+  # config.aws_access_key_id = "abc123"
+  # config.aws_secret_key = "abc123"
+  # config.s3_bucket = "mybucket"
+end
